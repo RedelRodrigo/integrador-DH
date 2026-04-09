71c82b449fd3feb63de83f7298b3f9d76e8719e1
RedelRodrigo <RedelRodrigoA@gmail.com>
2026-04-09 09:04:24 -0300

    Landing Page

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 w-full bg-[#201F22] text-left text-[#C1FD35] py-4 px-10 h-16 flex items-center ">
+      © 2022 Digital Money House
+    </div>
+  );
+};
+
+export default Footer;
